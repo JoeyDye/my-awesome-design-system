08e7825ff81c7d50148397999eba24e0a56b21d4
Joey Dye <joey.m.dye@gmail.com>
2020-05-13 08:42:00 -0500

    feat: add utils

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
-import { PrimaryButton } from './components/Buttons'
+import {
+  PrimaryButton,
+  SecondaryButton,
+  TertiaryButton,
+} from './components/Buttons'
 
 const App = () => {
   return (
     <div>
       <PrimaryButton>Submit</PrimaryButton>
+      <SecondaryButton>Submit</SecondaryButton>
+      <TertiaryButton>Submit</TertiaryButton>
     </div>
   )
 }
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,19 +1,43 @@
 import styled from 'styled-components'
+import { defaultTheme, typeScale } from '../utils'
 
 export const Button = styled.button`
   min-width: 100px;
-  border-radius: 2px;
+  border-radius: 50px;
   padding: 12px 24px;
-  font-size: 1rem;
+  font-size: ${typeScale.paragraph};
   font-family: 'Montserrat', sans-serif;
   cursor: pointer;
 `
 
 export const PrimaryButton = styled(Button)`
-  background-color: red;
+  background-color: ${defaultTheme.primaryColor};
   border: none;
   color: white;
-  font-size: 1rem;
+
+  &:hover {
+    background: ${defaultTheme.primaryColorHover};
+  }
 `
 
-export default PrimaryButton
+export const SecondaryButton = styled(Button)`
+  background-color: white;
+  border: 1px solid ${defaultTheme.primaryColor};
+  color: ${defaultTheme.primaryColor};
+
+  &:hover {
+    background-color: ${defaultTheme.primaryColor};
+    color: white;
+  }
+`
+
+export const TertiaryButton = styled(Button)`
+  background-color: white;
+  border: none;
+  color: ${defaultTheme.primaryColor};
+
+  &:hover {
+    background-color: ${defaultTheme.primaryColor};
+    color: white;
+  }
+`
